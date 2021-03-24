11b85f4dfb7159c62a814f4a893b2744a1072529
cfelintro <80720186+cfelintro@users.noreply.github.com>
2021-03-24 16:48:36 -0300

    Update Cadastro_de_Pessoas.py
    Codigo V02 : Re-escrito melhorando legibilidade e logica geral

--- Cadastro_de_Pessoas.py
+++ Cadastro_de_Pessoas.py
@@ -1,103 +1,77 @@
+#CADASTRO V02 - CODIGO RE-ESCRITO DE FORMA MAIS LEGIVEL
 
-#Listas de Dados:
-index = [0,]
-nomes = ['NOME']
-sobrenomes = ['SOBRENOME']
-telefones = [000]
-cpfs = [00000000000]
+database = []
+lista_parametros = ['nome','sobrenome','telefone','cpf']
 
-#Dicionario de dados:
-database = {
-    'index':index,
-    'nome':nomes,
-    'sobrenome':sobrenomes,
-    'telefone':telefones,
-    'cpf':cpfs
-}
+
 
 #MENU INICIAL - Chama as demais funções ou termina o programa:
-
 def main_menu():
   print()
   selection = str(input('O que deseja fazer? :')).capitalize()
+
   if selection == 'C' or selection == 'Cadastrar':
-    cadastrar()
+    cadastro()
+
   elif selection == 'L' or selection == 'Listar':
     listar()
+
   elif selection == 'R' or selection == 'Remover':
     remover()
+
   elif selection == 'E' or selection == 'Editar':
     editar()
+
   elif selection == 'T':
     terminate()
+
   else:
-    print()
     print('Selecao invalida')
     print()
     main_menu()
 
-#FIM DE PROGRAMA
-def terminate():
-  print('FIM DA EXECUÇÃO')
-
-#FUNÇÃO BUSCA - Utilizada para procurar valores para as funções remover e editar
-def busca(nome_busca, sobrenome_busca):
-  index_busca = -1
-
-  for numero in index:
-    nome = database["nome"][numero-1]
-    sobrenome = database["sobrenome"][numero-1]
-
-    if nome == nome_busca and sobrenome == sobrenome_busca:
-      index_busca = numero
-
-  return(index_busca)
 
 
-#FUNÇÃO CADASTRO - Adiciona novos cadastros na base de dados
-def cadastrar():
-
-  print('***CADASTRO***')
-  print()
-
-  nome = str(input('Nome: ')).upper()
+#FUNÇÃO CADASTRO
+def cadastro():
+  print('***CADASTRAR***')
+  nome = input('NOME: ').upper()
   if nome == 'CANCELAR':
     main_menu()
-  else:
-    sobrenome = str(input('Sobrenome: ')).upper()
-    if sobrenome == 'CANCELAR':
-      main_menu()
-    else:
-      telefone = str(input('Telefone: ')).upper()
-      if telefone == 'CANCELAR':
-        main_menu()
-      else:
-        cpf = str(input('CPF: ')).upper()
-        if cpf == 'CANCELAR':
-          main_menu()
-        else:
-          index.append(len(index))
-          sobrenomes.append(sobrenome)
-          telefones.append(telefone)    
-          nomes.append(nome)
-          cpfs.append(cpf)
 
-          print()
-          print('CADASTRO CONCLUÍDO COM SUCESSO!')
+  sobrenome = input('SOBRENOME: ').upper()
+  if sobrenome == 'CANCELAR':
+    main_menu()
 
-          main_menu()
+  telefone =  input('TELEFONE: ').upper()
+  if telefone == 'CANCELAR':
+    main_menu()
+  
+  cpf =   input('CPF: ').upper()
+  if cpf == 'CANCELAR':
+    main_menu()
+
+  cadastro = {
+      "nome":nome, 
+      "sobrenome":sobrenome, 
+      "telefone":telefone, 
+      "cpf":cpf
+      }
+
+  database.append(cadastro)
+  print('CADASTRO CONCLUÍDO COM SUCESSO!')
+  main_menu()
 
 
-#FUNÇÃO LISTA - Lista todos os nomes e sobrenomes na base de dados
-#Pode ser atualizada para exibir mais dados
 
+#FUNÇÃO LISTAR NOMES E SOBRENOMES NO SISTEMA
 def listar():
   print('***LISTAR***')
   print('Aspessoas cadastradas no sistema são:')
   print()
 
-  for num in index: 
-    print(f'{database["index"][num]} - {database["nome"][num]} {database["sobrenome"][num]}')
+  for item_cadastrado in database: 
+    print(item_cadastrado["nome"], item_cadastrado["sobrenome"])
 
   print()
   print('LISTA CONCLUÍDA COM SUCESSO!')
@@ -105,90 +79,74 @@ def listar():
   main_menu()
 
 
-#FUNÇÃO REMOVER - Utiliza a função busca e caso retorne um valor existente
-#remove o mesmo da base de dados
+
+#FUNÇÃO REMOVER - Busca o nome e sobrenome na lista cadastrada e caso existe
+#remove o mesmo
 def remover():
   print('***REMOVER***')
   print()
 
-  nome = str(input('Nome do cadastro a remover: ')).upper()
-  if nome == 'CANCELAR':
+  nome_remover = str(input('Nome completo do cadastro a remover: ')).upper()
+  if nome_remover == 'CANCELAR':
     main_menu()
+  
+  nome_remover = nome_remover.split(' ')
+  nome_encontrado = False
+
+  for item_cadastrado in database:
+
+    if item_cadastrado["nome"] == nome_remover[0] and item_cadastrado["sobrenome"] == nome_remover[1]:
+      database.remove(item_cadastrado)
+      nome_encontrado = True
+
+  if nome_encontrado == True:
+    print('CADASTRO REMOVIDO COM SUCESSO')
   else:
-    sobrenome = str(input('Sobrenome do cadastro a remover: ')).upper()
-    if sobrenome == 'CANCELAR':
-      main_menu()
-    else:
-      num_index = busca(nome, sobrenome)
-      if num_index == -1:
-        print('Cadastro não encontrado')
-        print()
-        remover()
-      else:
-        for numero in index:
-          nome = database["nome"][numero-1]
-          sobrenome = database["sobrenome"][numero-1]
-          telefone = database["telefone"][numero-1]
-          cpf =  database["cpf"][numero-1]
-
-          if numero == num_index:
-            index.remove(len(index)-1)
-            nomes.remove(nome)
-            sobrenomes.remove(sobrenome)
-            telefones.remove(telefone)
-            cpfs.remove(cpf)
-
-  print('CADASTRO REMOVIDO COM SUCESSO')
+    print('CADASTRO NÃO ENCONTRADO')
+    remover()
 
   main_menu()
- 
 
-#FUNÇÃO EDITAR - Utiliza a função busca e caso retorne um valor existente
-#permite alterar cadaum dos valores da base de dados
 
+
+#FUNÇÃO EDITAR - Busca nome e sobrenome na lista e caso exista permite editar
 def editar():
   print('***EDITAR***')
   print()
 
-  nome = str(input('Nome do cadastro a ser editado: ')).upper()
-  if nome == 'CANCELAR':
+  nome_editar = str(input('Nome completo do cadastro a editar: ')).upper()
+  if nome_editar == 'CANCELAR':
     main_menu()
+  
+  nome_editar = nome_editar.split(' ')
+  nome_encontrado = False
+
+  for item_cadastrado in database:
+
+    if item_cadastrado["nome"] == nome_editar[0] and item_cadastrado["sobrenome"] == nome_editar[1]:
+      for parametro in lista_parametros:
+        print(parametro.upper(), 'ATUAL: ',item_cadastrado[parametro])
+        novo_valor = input(f'NOVO {parametro.upper()}: ').upper()
+
+        if novo_valor != '':
+          item_cadastrado[parametro] = novo_valor
+
+      nome_encontrado = True
+
+  if nome_encontrado == True:
+    print('CADASTRO EDITADO COM SUCESSO')
   else:
-    sobrenome = str(input('Sobrenome do cadastro a ser editado: ')).upper()
-    if sobrenome == 'CANCELAR':
-      main_menu()
-    else:
-      num_index = busca(nome, sobrenome)
-      if num_index == -1:
-        print('Cadastro não encontrado')
-        print()
-        editar()
-      else:
-        for numero in index:
-          nome = database["nome"][numero-1]
-          sobrenome = database["sobrenome"][numero-1]
-          telefone = database["telefone"][numero-1]
-          cpf =  database["cpf"][numero-1]
-
-          if numero == num_index:
-            novo_nome = str(input('Nome: ')).upper()
-            novo_sobrenome = str(input('Sobrenome: ')).upper()
-            novo_telefone = str(input('Telefone: ')).upper()
-            novo_cpf = str(input('CPF: ')).upper()
-            
-            if novo_nome != '':
-              nomes[numero-1] = novo_nome
-            if novo_sobrenome != '':
-              sobrenomes[numero-1] = novo_sobrenome
-            if novo_telefone != '':
-              telefones[numero-1] = novo_telefone
-            if novo_cpf != '':
-              cpfs[numero-1] = novo_cpf
-
-  print('CADASTRO EDITADO COM SUCESSO')
-
+    print('CADASTRO NÃO ENCONTRADO')
+    editar()
   main_menu()
 
-#Execução da função MENU INICIAL para inicio do programa
+
+
+#FIM DE PROGRAMA
+def terminate():
+  print('FIM DA EXECUÇÃO')
+  exit()
+
+
 
 main_menu()
